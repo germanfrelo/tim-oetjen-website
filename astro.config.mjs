78d0d9bc94e1b73d694c2a39558937514f2643c4
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2026-05-25 11:21:19 +0200

    feat: Switch primary domain to timoetjen.eu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://traductorautonomo.es",
+	site: "https://timoetjen.eu",
 	devToolbar: {
 		enabled: false,
 	},
