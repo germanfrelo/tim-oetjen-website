3211ebfeb20f97c5eca2e4f0ad1e38d8d2e48b43
Germán Freixinós <14282156+germanfrelo@users.noreply.github.com>
2026-05-25 11:47:24 +0200

    style: Reformat files per updated Prettier config

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE =
-	"Traducteur indépendant - Tim Oetjen - Traduction EN / NL / ES > FR";
-export const SITE_DESCRIPTION =
-	"Traducteur indépendant de confiance pour gérer tous vos projets de traduction de l’anglais, du néerlandais et de l’espagnol vers le français.";
+export const SITE_TITLE = "Traducteur indépendant - Tim Oetjen - Traduction EN / NL / ES > FR";
+export const SITE_DESCRIPTION = "Traducteur indépendant de confiance pour gérer tous vos projets de traduction de l’anglais, du néerlandais et de l’espagnol vers le français.";
 export const CONTACT_EMAIL = "hello@timoetjen.eu";
